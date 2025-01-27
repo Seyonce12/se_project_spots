6395d43e6af59ce1cf7645636f4cbf74aa40fcb3
Seyonce12 <sikdersabrina@gmail.com>
2025-01-27 08:01:19 -0500

    final stage 1/3 and 2/3

--- scripts/index.js
+++ scripts/index.js
@@ -24,14 +24,25 @@ const initialCards = [
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
     },
   ];
-  
- 
+
+//modal buttons
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-modal");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 
+//profile elements
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__description");
+
+//profile form elements
+const profileFormElement = document.querySelector('form[name="edit-profile"]');
+const nameInput = profileFormElement.querySelector('input[id="name"]');
+const jobInput = profileFormElement.querySelector('input[id="description"]');
+
+//functions
 function openModal() {
   editModal.classList.add("modal_opened");
+  getCurrentProfileValues();
 }
 
 function closeModal() {
@@ -40,4 +51,21 @@ function closeModal() {
 
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
-  
+
+function getCurrentProfileValues()
+{
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
+}
+
+function handleProfileFormSubmit(evt){
+  evt.preventDefault();
+  const nameValue = nameInput.value;
+  const jobValue = jobInput.value;
+  profileNameElement.textContent = nameValue.trim();
+  profileJobElement.textContent = jobValue.trim();
+  //close modal
+  closeModal();
+}
+
+profileFormElement.addEventListener('submit',handleProfileFormSubmit);
