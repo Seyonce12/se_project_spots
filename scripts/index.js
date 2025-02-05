2648798bef7928f1e6b8cc1dae97729775b7397c
Seyonce12 <sikdersabrina@gmail.com>
2025-02-05 07:20:24 -0500

    card add and delete logics

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,38 @@
 const initialCards = [
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-      name: "Val Thorens",
-    },
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-      name: "Restaurant terrace",
-    },
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-      name: "An outdoor cafe",
-    },
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-      name: "A very long bridge, over the forest and through the trees",
-    },
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-      name: "Tunnel with morning light",
-    },
-    {
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-      name: "Mountain house",
-    },
-  ];
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    name: "Val Thorens",
+  },
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    name: "Restaurant terrace",
+  },
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    name: "An outdoor cafe",
+  },
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    name: "A very long bridge, over the forest and through the trees",
+  },
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    name: "Tunnel with morning light",
+  },
+  {
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    name: "Mountain house",
+  },
+];
 
 //modal buttons
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const cardModalBtn = document.querySelector(".profile__add-btn");
 const editModal = document.querySelector("#edit-modal");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+
+const cardModal = document.querySelector("#add-card-modal");
+const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 
 //profile elements
 const profileNameElement = document.querySelector(".profile__name");
@@ -36,64 +40,100 @@ const profileJobElement = document.querySelector(".profile__description");
 
 //profile form elements
 const editFormElement = document.querySelector('form[name="edit-profile"]');
-const nameInput = document.querySelector("#profile-name-input");
-const jobInput = document.querySelector("#profile-description-input");
+const cardFormElement = document.querySelector('form[name="add-card-form"]');
+const nameInput = editModal.querySelector("#profile-name-input");
+const jobInput = editModal.querySelector("#profile-description-input");
+const cardNameInput = cardModal.querySelector("#add-card-name-input");
+const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
 //functions
-function openModal() {
-  editModal.classList.add("modal_opened");
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
-function closeModal() {
-  editModal.classList.remove("modal_opened");
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
-profileEditButton.addEventListener("click", function(){
+profileEditButton.addEventListener("click", function () {
   getCurrentProfileValues();
-  openModal();
+  openModal(editModal);
 });
 
-editModalCloseBtn.addEventListener("click", closeModal);
+editModalCloseBtn.addEventListener("click", () => {
+  closeModal(editModal);
+});
 
-function getCurrentProfileValues()
-{
+cardModalBtn.addEventListener("click", function () {
+  cardLinkInput.value = "";
+  cardNameInput.value = "";
+  openModal(cardModal);
+});
+
+cardModalCloseBtn.addEventListener("click", () => {
+  closeModal(cardModal);
+});
+
+function getCurrentProfileValues() {
   nameInput.value = profileNameElement.textContent;
   jobInput.value = profileJobElement.textContent;
 }
 
-function handleProfileFormSubmit(evt){
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   const nameValue = nameInput.value;
   const jobValue = jobInput.value;
   profileNameElement.textContent = nameValue.trim();
   profileJobElement.textContent = jobValue.trim();
   //close modal
-  closeModal();
+  closeModal(editModal);
 }
 
-editFormElement.addEventListener('submit',handleProfileFormSubmit);
+function handleAddCardSubmit(e) {
+  e.preventDefault();
+  const cardElement = getCardElement({
+    name: cardNameInput.value,
+    link: cardLinkInput.value,
+  });
+  cardsList.prepend(cardElement);
+  closeModal(cardModal);
+}
+
+editFormElement.addEventListener("submit", handleProfileFormSubmit);
+cardFormElement.addEventListener("submit", handleAddCardSubmit);
 
 //card template
 const cardTemplate = document.querySelector("#card-template");
 
 //function to fill the cards
 function getCardElement(data) {
-  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
   const cardNameEl = cardElement.querySelector(".card__title");
   cardNameEl.textContent = data.name;
   const cardImageEl = cardElement.querySelector(".card__image");
   cardImageEl.src = data.link;
-  cardImageEl.alt = data.name || 'No Image';
+  cardImageEl.alt = data.name || "No Image";
   const likeButtonEl = cardElement.querySelector(".card__like-btn");
+  const deleteButtonEl = cardElement.querySelector(".card__delete-btn");
+
   likeButtonEl.setAttribute("aria-label", "Like " + data.name);
+  likeButtonEl.addEventListener("click", () => {
+    likeButtonEl.classList.toggle("card__like-btn_liked");
+  });
+
+  deleteButtonEl.addEventListener("click", (e) => {
+    e.target.closest(".card").remove();
+  });
   return cardElement;
 }
 
 //card parent ul element
-const cardsList = document.querySelector('.cards__list');
+const cardsList = document.querySelector(".cards__list");
 
 // Loop through the initialCards array and append each card to the parent
-initialCards.forEach(cardData => {
+initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
-  cardsList.prepend(cardElement);
+  cardsList.append(cardElement);
 });
